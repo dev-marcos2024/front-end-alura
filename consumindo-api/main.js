b7f42fe384875c575a35746606538a80924bd7a8
Marcos Roberto <dev_marcos2024@outlook.com>
2024-10-03 15:17:25 -0300

    Finalizando consumindo api

--- consumindo-api/main.js
+++ consumindo-api/main.js
@@ -1,11 +1,29 @@
+// VARIAVEIS
+let inputCep = document.getElementById('cep');
+let cepInvalido = document.getElementById('erro__cep')
 
+// EVENTOS
+inputCep.addEventListener('focusout', ()=> buscarEndereco(inputCep.value));
+inputCep.addEventListener('focus', ()=> cepInvalido.style.display = 'none');
+
+// FUNÇÕES
 async function buscarEndereco(cep){
     
-    let retorno = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
-    let endereco = await retorno.json()
+    try {
+        let retorno = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+        let endereco = await retorno.json();
 
-    console.log(endereco)
+        if(endereco.erro){
+            throw Error("O cep digitado nao existe!");
+        }
+
+        console.log(endereco);
+        return endereco
+    } catch (erro) {
+        cepInvalido.querySelector('p').innerHTML = erro
+        cepInvalido.style.display = 'flex'
+    }
 }
 
 
-buscarEndereco('01001000')
+
